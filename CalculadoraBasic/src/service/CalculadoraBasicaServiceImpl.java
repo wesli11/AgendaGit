146abf0177430dba3bb45c26f180c44e010d344c
Wladi <Wladi@LAPTOP-AQASP5CE>
2021-03-02 22:29:56 +0100

    copia original del proyecto

---
+++ CalculadoraBasic/src/service/CalculadoraBasicaServiceImpl.java
@@ -0,0 +1,11 @@
+package service;
+
+public class CalculadoraBasicaServiceImpl implements CalculadoraBasicaService{
+
+	@Override
+	public int sumar(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
